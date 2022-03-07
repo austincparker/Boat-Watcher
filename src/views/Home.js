6df9f6988d55c84d533677a7afdc9d6cad4a4ade
Austin Parker <austincparker@gmail.com>
2022-03-07 17:34:46 -0600

    dummy data and get all boats

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { signOutUser } from '../api/auth';
 
 export default function Home() {
   return (
     <div>
       <h1>Boat Watcher!</h1>
+      <button type="button" onClick={signOutUser} color="danger">
+        Log Out
+      </button>
     </div>
   );
 }
